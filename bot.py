0cadd649f8a4f7de679d518f6ec168e3573a03e0
KuonRen <h4sukun83.stream@gmail.com>
2025-03-06 00:58:32 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 # Botの設定
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = "DISCORD_TOKEN"
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
